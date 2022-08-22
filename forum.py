3a5031dcb89c0ee6899dc75b8c595829661b0e04
overd <over.developer13@gmail.com>
2022-08-22 02:01:51 -0300

    organizando documentação do código

--- forum.py
+++ forum.py
@@ -1,14 +1,18 @@
 from random import sample
 
 
-# Embaralha a string passada como parametro
 def shuffle_word(word):
+    """
+    Embaralha a string passada como parametro
+    """
     return ''.join(sample(word, len(word)))
 
 
-# Retorna um array com a tabuada do '1' ao '10' do número passado como parametro
-# Caso o número passado como parametro não seja um número retorna uma mensagem de erro
 def multiplication_table(number):
+    """
+    Retorna um array com a tabuada do '1' ao '10' do número passado como parametro
+    Caso o número passado como parametro não seja um número retorna uma mensagem de erro
+    """
     table = []
 
     num = int(number)
